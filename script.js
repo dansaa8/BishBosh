43bb6bb68968133778122a84cbadda5c3cb5aca4
dansaa8 <dansaa8@protonmail.com>
2024-10-15 16:54:14 +0200

    Create a form input and retrieve loopcount, bishdivisor and boshdivisor. Use those values to run BishBosh method and print value in an ouput div.

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-BishBosh(3, 4);
+const form = document.querySelector('form');
+const output = document.querySelector('#output');
 
-function BishBosh(divisor1, divisor2) {
-  for (let i = 1; i < 100; i++) {
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  const iterationsValue = formData.get('iterations');
+  const bishValue = formData.get('bish-input');
+  const boshValue = formData.get('bosh-input');
+
+  e.target.reset();
+
+  const result = BishBosh(iterationsValue, bishValue, boshValue);
+  const textNode = document.createTextNode(result);
+  const p = document.createElement('p');
+  p.appendChild(textNode);
+  
+  output.appendChild(p);
+});
+
+function BishBosh(iterations, bishDivisor, boshDivisor) {
+  let result = '';
+  for (let i = 1; i <= iterations; i++) {
     let message = '';
-    if (i % divisor1 === 0 && i % divisor2 === 0) {
-      message += 'Bish-Bosh';
-    } else if (i % divisor1 === 0) {
-      message += 'Bish';
-    } else if (i % divisor2 === 0) {
-      message += 'Bosh';
-    }
-    if (message === '') {
-      console.log(i);
-    } else {
-      console.log(message);
-    }
+
+    if (i % bishDivisor === 0 && i % boshDivisor === 0) message += 'Bish-Bosh';
+    else if (i % bishDivisor === 0) message += 'Bish';
+    else if (i % boshDivisor === 0) message += 'Bosh';
+
+    if (message === '') message += i;
+
+    if (i < iterations) message += ', ';
+
+    result += message;
   }
+  return result;
 }
